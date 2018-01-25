7a37c29334f8ec9c29f5983207b0b65840f4f221
Chethiya Abeysinghe <chethiya@odoc.life>
2018-01-25 08:44:11 +0530

    fix

--- weya.ts
+++ weya.ts
@@ -4,17 +4,15 @@ const TAGS = {
   html: "a abbr address article aside audio b bdi bdo blockquote body button canvas caption cite code colgroup datalist dd del details dfn div dl dt em fieldset figcaption figure footer form h1 h2 h3 h4 h5 h6 head header hgroup html i iframe ins kbd label legend li main map mark menu meter nav noscript object ol optgroup option output p pre progress q rp rt ruby s samp script section select small span strong style sub summary sup table tbody td textarea tfoot th thead time title tr u ul video",
   htmlVoid: "area base br col command embed hr img input keygen link meta param source track wbr"
 };
-let doc = undefined;
-let WEYA_DOM: any = null;
-if (typeof window !== 'undefined' && window.document) {
-  doc = document
-  WEYA_DOM = domAPICreate()
-} else {
-  doc = undefined
-}
+
+let WEYA_DOM: any = undefined;
 const API = {
-  document: doc as Document
+  document: undefined as any
 };
+if (typeof window !== 'undefined' && window.document) {
+  API.document = document
+  WEYA_DOM = domAPICreate()
+}
 
 export type WeyaTemplateFunction = ($: WeyaHelper) => HTMLElement | void
 export type WeyaElementArg = (string | AttributesInterface | WeyaTemplateFunction)
